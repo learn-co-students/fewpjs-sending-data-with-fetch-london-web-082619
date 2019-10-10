63c62bf5d5d00579752b9907e2b46140c5bb209b
github username <github email address>
2019-10-10 10:23:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+const body = document.querySelector("body");
+
+function submitData(name, email) {
+  const URL = "http://localhost:3000/users"
+  const user = {
+    name: name,
+    email: email,
+  };
+  const requestConfig = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(user)
+  };
+
+  return fetch(URL, requestConfig)
+    .then(response => response.json())
+    .then(json => displayId(json))
+    .catch(error => displayError(error));
+}
+
+function displayId(json) {
+  const idSpan = document.createElement("span");
+  idSpan.textContent = json.id;
+  body.appendChild(idSpan);
+}
+
+function displayError(error) {
+  const errorSpan = document.createElement("span");
+  errorSpan.textContent = error.message;
+  body.appendChild(errorSpan);
+}
