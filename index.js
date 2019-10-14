7207febf8fbc7ce1972e342b1b5d9d6a5651f554
ck2e14 <chriswkennedy@icloud.com>
2019-10-14 17:14:32 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(username, useremail){
+    let formData = {
+        name: username,
+        email: useremail
+    };
+
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObject)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(object){
+            console.log(object)
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error){
+            document.body.innerHTML = error.message
+        })
+    };
+
