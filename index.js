03233bd41efc2a294babe6d29e03ad9e50f6ff3e
J-B Garway-Templeman <jbgarwayt@gmail.com>
2019-10-14 14:14:55 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+import { parse } from "url";
+
 // Add your code here
+
+function submitData (name, email) {
+    // let newData = {name: name, email: email}
+    // let configurationObject = {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json"
+    //     },
+    //     body: JSON.stringify(newData)
+    // }
+
+    return fetch ('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name, 
+            email
+        })
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+           document.body.innerHTML = object['id']
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        });
+}
+
+// submitData("will", "will@gmail.com").then( resp => console.log(resp))
